d97e39a2be76657439c37c80dcc24bccc2bb5acb
LeoCie <leon.cieslik@softwire.com>
2022-01-17 17:31:20 +0000

    ex-3: Add integration test

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,32 +4,34 @@ from todo_app.data.trello_items import get_items, create_item, delete_item, chan
 from todo_app.flask_config import Config
 from todo_app.models.ViewModel import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
+    @app.route('/')
+    def index():
+        items = get_items()
+        items = sorted(items, key=lambda item: item.title)
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
 
-@app.route('/')
-def index():
-    items = get_items()
-    items = sorted(items, key=lambda item: item.title)
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/add', methods = ['POST'])
+    def addItem():
+        title = request.form.get('title')
+        create_item(title)
+        return redirect('/')
 
-@app.route('/add', methods = ['POST'])
-def addItem():
-    title = request.form.get('title')
-    create_item(title)
-    return redirect('/')
+    @app.route('/delete', methods = ['POST'])
+    def deleteItem():
+        item_id = request.form.get('item_id')
+        delete_item(item_id)
+        return redirect('/')
 
-@app.route('/delete', methods = ['POST'])
-def deleteItem():
-    item_id = request.form.get('item_id')
-    delete_item(item_id)
-    return redirect('/')
+    @app.route('/changeStatus', methods = ['POST'])
+    def changeItemStatus():
+        item_id = request.form.get('item_id')
+        new_status = request.form.get('status')
+        change_item_status(item_id, new_status)
+        return redirect('/')
 
-@app.route('/changeStatus', methods = ['POST'])
-def changeItemStatus():
-    item_id = request.form.get('item_id')
-    new_status = request.form.get('status')
-    change_item_status(item_id, new_status)
-    return redirect('/')
+    return app
